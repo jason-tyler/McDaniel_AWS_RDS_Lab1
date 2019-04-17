effa176dd3603b680ab681ffaae4facd4ed7e46d
jason-tyler <jasonetyler@yahoo.com>
2019-04-17 18:32:08 -0400

    Update owner.sql

--- src/tables/owner.sql
+++ src/tables/owner.sql
@@ -1,4 +1,4 @@
-CREATE TABLE IF NOT EXISTS patient_type (
+CREATE TABLE IF NOT EXISTS owner (
     id INT AUTO_INCREMENT,
     first_name VARCHAR(255) NOT NULL,
     last_name VARCHAR(255) NOT NULL,
